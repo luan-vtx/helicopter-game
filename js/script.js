f98611664484617aa4331c04bdbf3b389e9677d0
Luan Xavier <luantenorio3@gmail.com>
2022-01-17 12:35:42 -0300

    feat: add comands up and down to the player helicopter

--- js/script.js
+++ js/script.js
@@ -1,33 +1,59 @@
-function start() { // Inicio da função start()
-
+function start() {
 	$("#inicio").hide();
-	
 	$("#fundoGame").append("<div id='jogador' class='anima1'></div>");
 	$("#fundoGame").append("<div id='inimigo1' class='anima2'></div>");
 	$("#fundoGame").append("<div id='inimigo2'></div>");
 	$("#fundoGame").append("<div id='amigo' class='anima3'></div>");
 
 	//Principais variáveis do jogo
+	let jogo = {};
+
+	// os valores de cada chave desse objeto são os respectivos keycodes de cada uma das letras
+	let TECLA = {
+		W: 87,
+		S: 83,
+		D: 68
+	};
+
+	jogo.pressionou = [];
+
+	//Verifica se o usuário pressionou alguma tecla	
+	$(document).keydown(function(e){
+		jogo.pressionou[e.which] = true;
+	});
 	
-	var jogo = {}
+	$(document).keyup(function(e){
+		jogo.pressionou[e.which] = false;
+	});
 	
 	//Game Loop
-
 	jogo.timer = setInterval(loop,30);
-	
+
 	function loop() {
-	
-	movefundo();
-	
-	} // Fim da função loop()
+		movefundo();
+		movejogador();
+	}
 
 	//Função que movimenta o fundo do jogo
-	
 	function movefundo() {
-	
-	esquerda = parseInt($("#fundoGame").css("background-position"));
-	$("#fundoGame").css("background-position", esquerda - 5);
+		esquerda = parseInt($("#fundoGame").css("background-position"));
+		$("#fundoGame").css("background-position", esquerda - 5);
+	}
 
-	} // fim da função movefundo()
-
+	// função que movimenta o jogador
+	function movejogador() {
+		if (jogo.pressionou[TECLA.W]) {
+			var topo = parseInt($("#jogador").css("top"));
+			$("#jogador").css("top",topo-10);
+		}
+		
+		if (jogo.pressionou[TECLA.S]) {	
+			var topo = parseInt($("#jogador").css("top"));
+			$("#jogador").css("top",topo+10);	
+		}
+		
+		if (jogo.pressionou[TECLA.D]) {
+			//Chama função Disparo	
+		}
+	}
 } // Fim da função start
